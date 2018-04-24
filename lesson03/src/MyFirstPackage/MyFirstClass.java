e8ef872cee810b110a0741b48e9bc447553162c8
VictorAndronatiev <victor.andronatiev1@gmail.com>
2018-04-24 20:26:49 +0300

    Lectia 3

---
+++ lesson03/src/MyFirstPackage/MyFirstClass.java
@@ -0,0 +1,13 @@
+package MyFirstPackage;
+
+public class MyFirstClass {
+
+    public static void main (String[] args) {
+        int total=sum (3, 2);
+        System.out.print("My First java application");
+    }
+
+    public  static int sum(int x, int y) {
+        return x+y;
+    }
+}
