908bfe8abf8cbf08404c35ed7b236f49b8a4d2f0
VictorAndronatiev <victor.andronatiev1@gmail.com>
2018-04-26 19:31:21 +0300

    practice 4_01

---
+++ Lesson04/practices/Practice_04-1/src/practice_04_1/ShoppingCart.java
@@ -0,0 +1,15 @@
+/* Copyright © 2016 Oracle and/or its affiliates. All rights reserved. */
+
+package practice_04_1;
+
+public class ShoppingCart {
+    public static void main(String[] args){
+        // Declare and initialize String variables.  Do not initialize message yet.
+        String custName= "Mary Smith", itemDesc="Shirt";
+        String message="hhhh";
+        // Assign the message variable 
+        message = custName + " wants to purcase " + itemDesc;
+        // Print and run the code1
+        System.out.println(message);
+    }
+}
